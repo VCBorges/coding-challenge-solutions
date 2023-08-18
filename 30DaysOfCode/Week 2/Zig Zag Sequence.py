36f89bdab00e786dc5f0758a9a8afd82d26df9d7
VCBorges <vinicius.costa.borges98@gmail.com>
2023-08-18 20:47:49 -0300

    FINISHED - Week 2 - ZigZagSequence

--- 30DaysOfCode/Week 2/Zig Zag Sequence.py
+++ 30DaysOfCode/Week 2/Zig Zag Sequence.py
@@ -1,41 +1,24 @@
-def findZigZagSequence(a: list, n: int):
-    a.sort() # [1, 2, 3, 4, 5]
-    mid = int((n + 1)/2) # 3
-
-    print(f'before a[mid]: {a[mid]}')
-    print(f'before a[n-1]: {a[n-1]}')
-
-    
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int((n + 1)/2) - 1
     a[mid], a[n-1] = a[n-1], a[mid]
 
-    print(f'after a[mid]: {a[mid]}')
-    print(f'after a[n-1]: {a[n-1]}')
-    
-    print(f'after a: {a}')
     st = mid + 1
-    ed = n - 1
-
-    print(f'st: {st}')
-    print(f'ed: {ed}')
-
-    print(f'a[st]: {a[st]}')
-
-    a[4], a[4] = a[4], a[4]
-    
-    print(f'a: {a}')
-    
+    ed = n - 2
     while(st <= ed):
         a[st], a[ed] = a[ed], a[st]
         st = st + 1
-        ed = ed + 1
+        ed = ed - 1
 
-    # for i in range (n):
-    #     if i == n-1:
-    #         print(a[i])
-    #     else:
-    #         print(a[i], end = ' ')acker
-    # return
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i], end = ' ')
+    return
 
-a = [2,3,5,1,4]
-n = len(a)
-findZigZagSequence(a, n)
+test_cases = int(input())
+for cs in range (test_cases):
+    n = int(input())
+    a = list(map(int, input().split()))
+    findZigZagSequence(a, n)
