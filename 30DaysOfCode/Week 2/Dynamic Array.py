9c8511cb4239755fcc9a0c2165a2c8aad4647841
VCBorges <vinicius.costa.borges98@gmail.com>
2023-08-21 10:04:56 -0300

    STARTING - Week 2 - Dynamic Array

--- 30DaysOfCode/Week 2/Dynamic Array.py
+++ 30DaysOfCode/Week 2/Dynamic Array.py
@@ -1,18 +1,34 @@
 def dynamicArray(n, queries):
+    arr = [
+        [] for i in n
+    ]
+    
+    
+    
+    
+    
+    print(f'arr {arr}')
     # Write your code here
     pass
 
 
-first_multiple_input = input().rstrip().split()
+# first_multiple_input = input().rstrip().split()
 
-n = int(first_multiple_input[0])
+# n = int(first_multiple_input[0])
 
-q = int(first_multiple_input[1])
+# q = int(first_multiple_input[1])
 
-queries = []
+# queries = []
 
-for _ in range(q):
-    queries.append(list(map(int, input().rstrip().split())))
+# for _ in range(q):
+#     queries.append(list(map(int, input().rstrip().split())))
+
+
+# print(f'queries {queries}')    
+
+n = 2
+
+queries = [[1, 0, 5]]
 
 result = dynamicArray(n, queries)
 
