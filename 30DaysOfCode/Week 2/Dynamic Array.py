1cc77713b18bc3be45af3c82b7d96a9b58c7b255
VCBorges <vinicius.costa.borges98@gmail.com>
2023-08-22 08:44:42 -0300

    STILL - Week 2 - Dynamic Array

--- 30DaysOfCode/Week 2/Dynamic Array.py
+++ 30DaysOfCode/Week 2/Dynamic Array.py
@@ -1,15 +1,28 @@
-def dynamicArray(n, queries):
+def dynamicArray(n: int, queries: list[int]):
     arr = [
-        [] for i in n
+        [] for _ in range(n)
     ]
     
+    last_answer = 0
+    result = []
+    
+    for query in queries:
+        if query[0] == 1:
+            idx = ((query[1] ^ last_answer) % n)
+            arr[idx].append(query[2])
+        else:
+            idx = ((query[1] ^ last_answer) % n)
+            # print(f'last answer {last_answer}')
+            last_answer = idx
+            # result.append(last_answer)
+        # print(idx)
     
     
     
-    
+    print(result)
     print(f'arr {arr}')
-    # Write your code here
-    pass
+    
+    return arr
 
 
 # first_multiple_input = input().rstrip().split()
@@ -28,7 +41,13 @@ def dynamicArray(n, queries):
 
 n = 2
 
-queries = [[1, 0, 5]]
+queries = [
+    [1, 0, 5],
+    [1, 1, 7],
+    [1, 0, 3],
+    [2, 1, 0],
+    [2, 1, 1],
+]
 
 result = dynamicArray(n, queries)
 
