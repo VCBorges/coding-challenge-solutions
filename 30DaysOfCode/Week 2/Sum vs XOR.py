05f23872edd56ab38cd3b7aa813dc58dc4fbd872
VCBorges <vinicius.costa.borges98@gmail.com>
2023-08-22 21:50:57 -0300

    FINISHED - Week 2 - Sum vs XOR

--- 30DaysOfCode/Week 2/Sum vs XOR.py
+++ 30DaysOfCode/Week 2/Sum vs XOR.py
@@ -1,18 +1,18 @@
 
-
 def sumXor(n):
-    cont = 0
-    for x in range(n + 1):
-        if n + x is n ^ x:
-            cont += 1
+    # cont = 0
+    # for x in range(n + 1):
+    #     if n + x is n ^ x:
+    #         cont += 1
         
-    return cont
-
+    # return cont
+    unset_bits = bin(n).count('0') - 1 if n > 0 else 0
+    return 2**unset_bits
 
 
 # n = int(input().strip())
 
-n = 1099511627776
+n = 5
 
 result = sumXor(n)
 
