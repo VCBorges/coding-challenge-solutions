6f98e57ff42b148da89374023ff9e9332aaeef7e
VCBorges <vinicius.costa.borges98@gmail.com>
2024-05-28 22:46:33 -0300

    style: restructured repo folder layout

---
+++ src/data_structures/Arrays - DS.py
@@ -0,0 +1,8 @@
+def reverseArray(a: list[int]):
+    return [a[-i] for i in range(1, len(a) + 1)]
+
+arr_count = int(input().strip())
+
+arr = list(map(int, input().rstrip().split()))
+
+res = reverseArray(arr)
