5dbaede0f3901bd5fbc1894755fd6e207ffc8fe3
VCBorges <vinicius.costa.borges98@gmail.com>
2024-06-02 22:27:38 -0300

    left rotation

---
+++ src/data_structures/arrays/Left Rotation.py
@@ -0,0 +1,19 @@
+from rich import print
+
+def rotateLeft(d: int, arr: list[int]) -> list[int]:
+    output = [None for _ in range(len(arr))]
+    for i in range(len(arr)):
+        output[i - d] = arr[i]        
+    return output
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+d = int(first_multiple_input[1])
+
+arr = list(map(int, input().rstrip().split()))
+
+result = rotateLeft(d, arr)
+
+print(f'{result = }')
