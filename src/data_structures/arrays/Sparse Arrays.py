f4ea70daae2d0a21c4d3f38d912c0e723bd29eab
VCBorges <vinicius.costa.borges98@gmail.com>
2024-06-03 23:30:07 -0300

    sparse arrays

---
+++ src/data_structures/arrays/Sparse Arrays.py
@@ -0,0 +1,31 @@
+def matching_strings(
+    string_list: list[str], 
+    queries: list[str],
+) -> list[int]:
+    result = [0 for _ in range(len(queries))]
+    for i in range(len(queries)):
+        for j in range(len(string_list)):
+            if queries[i] == string_list[j]:
+                result[i] += 1
+    return result
+
+if __name__ == '__main__':
+    
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    
+    queries_count = int(input().strip())
+
+    queries = []
+    
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matching_strings(stringList, queries)
