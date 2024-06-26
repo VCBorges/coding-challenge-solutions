92e012ccb5da99cb882fbd2490ad0e98c7fedb64
VCBorges <vinicius.costa.borges98@gmail.com>
2024-06-25 22:03:51 -0300

    Luck Balance fixed

--- hackerrank/algorithms/greedy/Luck Balance.py
+++ hackerrank/algorithms/greedy/Luck Balance.py
@@ -1,5 +1,3 @@
-from rich import print
-
 def luckBalance(k: int, contests: list[list[int]]) -> int:
     luck_balance = 0
     loseble_important_contests = k
@@ -19,29 +17,15 @@ def luckBalance(k: int, contests: list[list[int]]) -> int:
     return luck_balance
 
 if __name__ == '__main__':
-    # first_multiple_input = input().rstrip().split()
+    first_multiple_input = input().rstrip().split()
     
-    first_multiple_input = '6 3'.split()
-
     n = int(first_multiple_input[0])
 
     k = int(first_multiple_input[1])
 
-    contests = [
-        [5, 1],
-        [2, 1],
-        [1, 1],
-        [8, 1],
-        [10, 0],
-        [5, 0],
-    ]
+    contests = []
 
-
-    # contests = []
-
-    # for _ in range(n):
-    #     contests.append(list(map(int, input().rstrip().split())))
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
 
     result = luckBalance(k, contests)
-    
-    print(f'{result = }')
