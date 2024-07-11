aaf635e5e02e2e4ce69c77cf850e55263ef59198
VCBorges <vinicius.costa.borges98@gmail.com>
2024-07-11 08:55:49 -0300

    Luck Balance

---
+++ hackerrank/algorithms/greedy/Luck_Balance.py
@@ -0,0 +1,31 @@
+def luckBalance(k: int, contests: list[list[int]]) -> int:
+    luck_balance = 0
+    loseble_important_contests = k
+    contests.sort(
+        key=lambda x: (x[1], x[0]), 
+        reverse=True,
+    )
+    for i in range(len(contests)):
+        contest_luck = contests[i][0]
+        contest_importance_rating = contests[i][1]
+        if loseble_important_contests > 0 or contest_importance_rating == 0:
+            luck_balance += contest_luck
+            loseble_important_contests -= 1
+        else:
+            luck_balance -= contest_luck
+    
+    return luck_balance
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+    
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    contests = []
+
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
+
+    result = luckBalance(k, contests)
